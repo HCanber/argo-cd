2ce99634ca25b8852f70b9e7136f404d9445aaac
Håkan Canberger <hakan.canberger@thomascook.se>
2019-10-29 17:00:44 +0100

    Update to helm v2.15.1
    Also verify checksum

--- hack/installers/install-helm-linux.sh
+++ hack/installers/install-helm-linux.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -eux -o pipefail
 
-[ -e $DOWNLOADS/helm.tar.gz ] || curl -sLf --retry 3 -o $DOWNLOADS/helm.tar.gz https://storage.googleapis.com/kubernetes-helm/helm-v2.13.1-linux-amd64.tar.gz
+[ -e $DOWNLOADS/helm.tar.gz ] || curl -sLf --retry 3 -o $DOWNLOADS/helm.tar.gz https://storage.googleapis.com/kubernetes-helm/helm-v2.15.1-linux-amd64.tar.gz
+echo "b4d366bd6625477b2954941aeb7b601946aa4226af6728e3a84eac4e62a84042  $DOWNLOADS/helm.tar.gz" | sha256sum -c
 tar -C /tmp/ -xf $DOWNLOADS/helm.tar.gz
 cp /tmp/linux-amd64/helm $BIN/helm
 helm version --client
